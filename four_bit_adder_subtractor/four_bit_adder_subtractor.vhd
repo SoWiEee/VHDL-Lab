360bbbc23f41d3d8800e95540977037cff33ada4
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2025-03-14 00:07:26 +0800

    Update four_bit_adder_subtractor.vhd

--- four_bit_adder_subtractor/four_bit_adder_subtractor.vhd
+++ four_bit_adder_subtractor/four_bit_adder_subtractor.vhd
@@ -1,30 +1,31 @@
-Library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric.std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
--- 4-bit +-
-Entity four_bit_adder_subtractor is 
-    port(
-        A, B : in std_logic_vector(3 downto 0);		-- 4-bit input
-        Cin, Op : in std_logic;							-- Op: add/sub
-        Sum : out std_logic_vector(3 downto 0);		-- 4-bit sum
-        Cout : out std_logic                       -- carry output
-    );
-end four_bit_adder_subtractor;
+-- 4b adder/suber --
+ENTITY AddSuber IS
+	Port(
+		Op, Cin: in std_logic;
+		A, B: in std_logic_vector(3 downto 0);
+		S: out std_logic_vector(3 downto 0);
+		Cout: out std_logic
+	);
+END AddSuber;
 
-
-Architecture Behavioral of four_bit_adder_subtractor is
+ARCHITECTURE Behavioral OF AddSuber IS
 begin
-    process(A, B, Cin)
-        variable SumUnsigned : unsigned(4 downto 0);  -- unsigned result (5-bit)
-    begin
-		  if Op = '1' then
-            sumUnsigned <= ('0'&unsigned(A)) + ('0'&unsigned(B)) + ('0'&Cin);
-        else
-            sumUnsigned <= ('0'&unsigned(A)) - ('0'&unsigned(B)) - ('0'&Cin);
-        end if;
-		        Cout <= std_logic(sumUnsigned(4));  -- sumUnsigned[4] (MSB)
-        sum <= std_logic_vector(sumUnsigned(3 downto 0));  -- sumUnsigned[3:0]
-    end process;
+	process(A, B, Cin, Op)
+		variable SumUnsigned : unsigned(4 downto 0);	-- result
+	begin
+		if Op = '1' then
+			-- add
+			sumUnsigned := ('0'&unsigned(A)) + ('0'&unsigned(B)) + ('0'&Cin);
+	  else
+			-- sub
+			sumUnsigned := ('0'&unsigned(A)) - ('0'&unsigned(B)) - ('0'&Cin);
+	  end if;
+			  
+	  Cout <= std_logic(sumUnsigned(4));
+	  S <= std_logic_vector(sumUnsigned(3 downto 0));
+	end process;
 end Behavioral;
-
