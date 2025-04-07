7646ce1340049d07e3a0ce0fc18af64ae42a0152
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2025-04-08 01:15:36 +0800

    Add files via upload

--- FreqDivider/FrequencyDivider.vhd
+++ FreqDivider/FrequencyDivider.vhd
@@ -1,33 +1,49 @@
 Library IEEE;
 USE IEEE.std_logic_1164.all;
+USE IEEE.numeric_std.all;
 
+entity FrequencyDivider is
+    --generic (divisor : integer := 2);
+    port (
+        clk_in    : in  std_logic;
+        clk_out8Hz : out std_logic;
+        clk_out4Hz : out std_logic;
+        clk_out2Hz : out std_logic;
+        clk_out1Hz : out std_logic
+    );
+end FrequencyDivider;
 
-ENTITY FrequencyDivider IS
-	Port(
-		clk_in		:in std_logic;
-		clk_div2		:out std_logic;
-		clk_div4		:out std_logic;
-		clk_div8		:out std_logic;
-		clk_div16	:out std_logic
-	);
-END FrequencyDivider;
+architecture arch of FrequencyDivider is
+    signal cnt : integer range 0 to 16 := 0;
 
+    signal clk_divided : std_logic_vector(3 downto 0) := "0000";	--array
 
-ARCHITECTURE Behavioral OF FrequencyDivider IS
-	signal q2, q4, q8, q16 : std_logic := '0';	-- trigger signal
-	BEGIN
+	begin
+	 -- 8Hz
 		process(clk_in)
-		 begin
-			  if rising_edge(clk_in) then
-					q2  <= not q2;
-					q4  <= q4 xor q2;
-					q8  <= q8 xor q4;
-					q16 <= q16 xor q8;
-			  end if;
+		begin
+			if falling_edge(clk_in) then
+				clk_divided(0) <= not clk_divided(0);	-- 8 Hz
+				cnt <= cnt + 1;
+				
+				if ((cnt+1) rem 2 = 0) then
+					clk_divided(1) <= not clk_divided(1);	-- 4 Hz
+				end if;
+				
+				if ((cnt+1) rem 4 = 0) then
+					clk_divided(2) <= not clk_divided(2);	-- 2 Hz
+				end if;
+				
+				if ((cnt+1) rem 8 = 0) then
+					clk_divided(3) <= not clk_divided(3);	-- 1 Hz
+				end if;
+			end if;
 		end process;
-		
-		clk_div2  <= q2;
-		clk_div4  <= q4;
-		clk_div8  <= q8;
-		clk_div16 <= q16;
-END Behavioral;
+
+	clk_out8Hz <= clk_divided(0);
+	clk_out4Hz <= clk_divided(1);
+	clk_out2Hz <= clk_divided(2);
+	clk_out1Hz <= clk_divided(3);
+
+end arch;
+
