b5155a5438ae25fc207cd80ce117f0a9c66c43c0
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2025-05-22 20:46:07 +0800

    Update ShiftReg.vhd

--- ShiftReg/ShiftReg.vhd
+++ ShiftReg/ShiftReg.vhd
@@ -3,30 +3,29 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ShiftReg IS
 	PORT(
-		rst, clk : in std_logic;
-		s : in std_logic_vector (1 downto 0);
-		ba: in std_logic_vector(3 downto 0);
-		srsi, slsi : in std_logic;
-		rc: inout std_logic_vector(3 downto 0)
+		RST, CLK   : in std_logic;
+		SEL	   : in std_logic_vector (1 downto 0);  -- select mode
+		BA	   : in std_logic_vector(3 downto 0);	-- load mode input
+		SRSI, SLSI : in std_logic;			-- right/left shift padding bit
+		RC	   : out std_logic_vector(3 downto 0)	-- output
 	);
-
 END ShiftReg;
 
 ARCHITECTURE b OF ShiftReg IS
 	BEGIN
-	PROCESS(clk)
+	PROCESS(CLK)
 	BEGIN
-		IF rising_edge(clk) then
-			IF rst = '1' then rc <= "0000";
+		IF rising_edge(CLK) then
+			IF RST = '1' then RC <= "0000";
 			ELSE
 				CASE s IS
-					WHEN "00" => rc <= rc;
-					WHEN "01" => rc(3) <= srsi; rc(2) <= rc(3);
-									 rc(1) <= rc(2); rc(0) <= rc(1);
+					WHEN "00" => RC <= RC;
+					WHEN "01" => RC(3) <= SRSI; RC(2) <= RC(3);
+						     RC(1) <= RC(2); RC(0) <= RC(1);
 				
-					WHEN "10" => rc(3) <= rc(2); rc(2) <= rc(1);
-									 rc(1) <= rc(0); rc(0) <= slsi;
-					WHEN "11" => rc <= ba;
+					WHEN "10" => RC(3) <= RC(2); RC(2) <= RC(1);
+						     RC(1) <= RC(0); RC(0) <= SLSI;
+					WHEN "11" => RC <= BA;
 					WHEN others => null;
 				END CASE;
 			END IF;
